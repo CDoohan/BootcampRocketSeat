5aba77a7ace3f88a3df17a5a12a10d8e3cc68a9e
CDoohan <pereiradoohan@gmail.com>
2019-09-10 13:54:31 -0300

    add table relationship between user and files

--- goBarber/src/routes.js
+++ goBarber/src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,8 +18,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true })
-})
+routes.post('/files', upload.single('file'), FileController.store)
 
 export default routes;
